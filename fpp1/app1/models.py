708caf11ab1ecf553efb6b4271ce9eba1bbcdb91
Yagub <tykva430@gmail.com>
2024-05-10 11:05:45 +0400

    changed model Stage

--- fpp1/app1/models.py
+++ fpp1/app1/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 
 class Stage(models.Model):
-    other_model = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    other_model = models.ForeignKey('self', verbose_name="Main stage link", on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=3000)
     question = models.TextField(blank=True, null=True)
     answer = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'Stage'
         verbose_name_plural = 'Stages'
+
+
 
